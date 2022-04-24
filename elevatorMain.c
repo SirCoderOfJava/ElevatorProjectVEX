5dc383907d2f63b27f22f1a10267638decea3544
Matt Marsteiner <31623939+SirCoderOfJava@users.noreply.github.com>
2022-04-24 18:07:20 -0400

    Update elevatorMain.c
    finish awaitinput()
    comment formatting

--- elevatorMain.c
+++ elevatorMain.c
@@ -9,12 +9,12 @@
 //encoder (shares axle with driver motor) count to determine floor
 //global timer (T1) that resets when the elevator is sent to a floor
 
-//Config schema
+//Config
 //motor that spins the winch is called 'motor'
 //encoder is called 'encoder'
 //call buttons are called 'callFN', where N [0,2] is the floor that the button is on
 //elevator buttons are called 'eFN', where N [0,2] is the floor that the button should send the elevator to
-//leds are called 'ledNK' where N [0,2] is the floor that the led is on and K [0,2] is the horizontal position of the led with 0 on the left and 2 on the right
+//leds are called 'ledN' where N [0,2] is the horizontal position of the led (0 - 1 - 2)
 
 //Part list
 //(1) control board
@@ -22,24 +22,24 @@
 //(1) encoder
 //(6) bump switches
 //(9) leds
+// Wire to connect leds
 
 /*
 TODO:
-finish awaitInput()
-figure out ports (we will likely have problems otherwise)
-finish goToFloor()
--implement routine to stop at the middle floor if the call button was pressed after we started moving but before we arrived
+-calibration program for encoder values
+-finish goToFloor()
+ -implement routine to stop at the middle floor if the call button was pressed after we started moving but before we arrived
 */
 
 void goToFloor(int destination); //Sends the elevator to the specified floor
-int awaitInput(); // Return ID of input
-void updateFloorDisplay(); //Updates the led display on every floor
-int parseInput(int in); //Take inputID and return destination to send the elevator to
+int awaitInput();                //Return ID of input
+void updateFloorDisplay();       //Updates the led display on every floor
+int parseInput(int in);          //Take inputID and return destination to send the elevator to
 
-const int encoderValues[3] = {0, 1, 2}; //TODO: Calibrate encoder values for specific floords
-int currentFloor;
-const int maxWaitLength = 5; //Maximum seconds to wait for input
-const int elevatorSpeed = 20; //Value from 0 to 127 that denotes the speed the motor that controls the elevator moves at
+const int encoderValues[3] = {0, 1, 2}; //TODO: Calibrate encoder values for specific floors
+int currentFloor;                       //Stores the floor # the elevator is on (or has most recently passed)
+const int maxWaitLength = 5;            //Maximum seconds to wait for input
+const int elevatorSpeed = 20;           //Value from 0 to 127 that denotes the speed the motor that controls the elevator moves at
 
 enum inputID {F0Call, F1Call, F2Call, ElevatorF0, ElevatorF1, ElevatorF2};
 enum floorID {F0, F1, F2};
@@ -47,49 +47,49 @@ enum floorID {F0, F1, F2};
 task main() {
 	//program always starts at ground floor
 	currentFloor = 0;
-	while(true) { //main control loop
-		int input = awaitInput(); //wait for input from the elevator
+	while(true) {                         //main control loop
+		int input = awaitInput();     //wait for input from the elevator
 		int dest = parseInput(input); //convert sensor input to destination
-		goToFloor(dest); //send elevator to the destination
+		goToFloor(dest);              //send elevator to the destination
 	}
 }
 
 void goToFloor(int destination) {
 	switch(destination) {
 		//cases to determine if elevator should move up or down (or not move at all)
-		case (destination == currentFloor): //elevator should stay in the same place
+		case (destination == currentFloor):                                      //elevator should stay in the same place
 			return;
-		case (destination < currentFloor): //elevator should move down
-			bool passedMiddle = false;  //Flag to prevent unnecessary processing by constantly updating leds
+		case (destination < currentFloor):                                       //elevator should move down
+			bool passedMiddle = false;                                       //Flag to prevent unnecessary processing by constantly updating leds
 			while(SensorValue[encoder] > encoderValues[destination]) { 
-				motor[motor] = -1 * elevatorSpeed; //Release the winch until the encoder value reads the value of the desired floor                  
-				if(!passedMiddle && //led has not already been updated
-				(currentFloor - destination == 2) && //Checks if there is a middle floor
-				(SensorValue[encoder] < encoderValues[destination + 1]))//Checks if the middle floor has been passed 
-				{ //Check if the elevator has passed the middle floor
-					currentFloor = destination + 1; //we have now passed the middle floor
-					updateFloorDisplay(); //update the floor display to show this
-					passedMiddle = true; //update flag so this statement doesn't unnecessarily run multiple times
+				motor[motor] = -1 * elevatorSpeed;                       //Release the winch until the encoder value reads the value of the desired floor                  
+				if(!passedMiddle &&                                      //led has not already been updated
+				(currentFloor - destination == 2) &&                     //Checks if there is a middle floor
+				(SensorValue[encoder] < encoderValues[destination + 1])) //Checks if the middle floor has been passed 
+				{                                                        //The above checks if the elevator has passed the middle floor
+					currentFloor = destination + 1;                  //update currentFloor to reflect that the elevator has passed the middle floor
+					updateFloorDisplay();                            //update the floor display to show this
+					passedMiddle = true;                             //update flag so this statement doesn't unnecessarily run multiple times
 				}
 			}
-			motor[motor] = 0; //Stop the elevator at the floor
-			currentFloor = destination; //Current floor is now the destination
+			motor[motor] = 0;                                                //Stop the elevator at the floor
+			currentFloor = destination;                                      //Current floor is now the destination
 			return;
-		case (destination > currentFloor): //elevator should move up
-			bool passedMiddle = false;  //Flag to prevent unnecessary processing by constantly updating leds
+		case (destination > currentFloor):                                       //elevator should move up
+			bool passedMiddle = false;                                       //Flag to prevent unnecessary processing by constantly updating leds
 			while(SensorValue[encoder] < encoderValues[destination]) { 
-				motor[motor] = elevatorSpeed; //Run the motor until the encoder value reads the value of the desired floor                  
-				if(!passedMiddle && //led has not already been updated
-				(destination - currentFloor == 2) && //Checks if there is a middle floor
-				(SensorValue[encoder] > encoderValues[destination - 1]))//Checks if the middle floor has been passed 
-				{ //Check if the elevator has passed the middle floor
-					currentFloor = destination - 1; //we have now passed the middle floor
-					updateFloorDisplay(); //update the floor display to show this
-					passedMiddle = true; //update flag so this statement doesn't unnecessarily run multiple times
+				motor[motor] = elevatorSpeed;                            //Run the motor until the encoder value reads the value of the desired floor                  
+				if(!passedMiddle &&                                      //led has not already been updated
+				(destination - currentFloor == 2) &&                     //Checks if there is a middle floor
+				(SensorValue[encoder] > encoderValues[destination - 1])) //Checks if the middle floor has been passed 
+				{                                                        //The above checks if the elevator has passed the middle floor
+					currentFloor = destination - 1;                  //we have now passed the middle floor
+					updateFloorDisplay();                            //update the floor display to show this
+					passedMiddle = true;                             //update flag so this statement doesn't unnecessarily run multiple times
 				}
 			}
-			motor[motor] = 0; //Stop the elevator at the floor
-			currentFloor = destination; //Current floor is now the destination
+			motor[motor] = 0;                                                //Stop the elevator at the floor
+			currentFloor = destination;                                      //Current floor is now the destination
 			return;
 	}
 }
@@ -97,13 +97,25 @@ void goToFloor(int destination) {
 int awaitInput() {
 	ClearTimer(T1); //clear the timer to start waiting
 	while(time1[T1] < maxWaitLength * 1000) {
-		//check every switch
-		//switches in elevator first, then call buttons
-		if(SensorValue[eF0] == 1) { //The button inside the elevator for F0 has been pressed
+		//check every switch, starting with the elevator buttons, then the call buttons
+		if(SensorValue[eF0] == 1) {           
 			return elevatorF0;
 		}
-		
-		//if a switch is pressed, return the associated inputID
+		else if(SensorValue[eF1] == 1) {
+			return elevatorF1;
+		}
+		else if(SensorValue[eF2] == 1) {
+			return elevatorF2;
+		}
+		else if(SensorValue[callF0] == 1) {
+			return F0call;
+		}
+		else if(SensorValue[callF1] == 1) {
+			return F1call;
+		}
+		else if(SensorValue[callF2] == 1) {
+			return F2call;
+		}
 	}
 	return F0; //If the timer expires and a return statement in the loop has not been triggered, return F0 to send the elevator to the ground floor
 }
@@ -124,43 +136,19 @@ int parseInput(int in) {
 void updateFloorDisplay() {
 	switch(currentFloor) {
 		case currentFloor == F0:  //if the elevator is on ground floor
-			turnLEDOn(led00);  
-			turnLEDOff(led01); 
-			turnLEDOff(led02);
-
-			turnLEDOn(led10);  
-			turnLEDOff(led11); 
-			turnLEDOff(led12);
-
-			turnLEDOn(led20);  
-			turnLEDOff(led21); 
-			turnLEDOff(led22);
+			turnLEDOn(led0);  
+			turnLEDOff(led1); 
+			turnLEDOff(led2);
 			return;
 		case currentFloor == F1:
-			turnLEDOff(led00);  
-			turnLEDOn(led01); 
-			turnLEDOff(led02);
-
-			turnLEDOff(led10);  
-			turnLEDOn(led11); 
-			turnLEDOff(led12);
-
-			turnLEDOff(led20);  
-			turnLEDOn(led21); 
-			turnLEDOff(led22);
+			turnLEDOff(led0);  
+			turnLEDOn(led1); 
+			turnLEDOff(led2);
 			return;
 		case currentFloor == F2:
-			turnLEDOff(led00);  
-			turnLEDOff(led01); 
-			turnLEDOn(led02);
-
-			turnLEDOff(led10);  
-			turnLEDOff(led11); 
-			turnLEDOn(led12);
-			
-			turnLEDOff(led20);  
-			turnLEDOff(led21); 
-			turnLEDOn(led22);
+			turnLEDOff(led0);  
+			turnLEDOff(led1); 
+			turnLEDOn(led2);
 			return;
 	}
 }
